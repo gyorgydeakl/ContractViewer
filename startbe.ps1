2d59b0bfe849068817559594ceb676c6a37547b9
gyorgydeakl <gyorgydeak.levente@gmail.com>
2025-08-14 15:28:14 +0200

    "Add startbe.ps1 script to concurrently run all API projects with process management and custom termination support."

---
+++ startbe.ps1
@@ -0,0 +1,24 @@
+# Start each project and keep process objects
+$procs = @()
+$procs += Start-Process dotnet -ArgumentList 'run --project ".\ContractViewerApi\UserApi\UserApi.csproj" --no-build' -PassThru
+$procs += Start-Process dotnet -ArgumentList 'run --project ".\ContractViewerApi\ContractListApi\ContractListApi.csproj" --no-build' -PassThru
+$procs += Start-Process dotnet -ArgumentList 'run --project ".\ContractViewerApi\ContractDetailsApi\ContractDetailsApi.csproj" --no-build' -PassThru
+$procs += Start-Process dotnet -ArgumentList 'run --project ".\ContractViewerApi\ContractViewerApi\ContractViewerApi.csproj" --no-build' -PassThru
+
+Write-Host "Press 'q' to stop all processes."
+
+while ($true) {
+    if ([Console]::KeyAvailable) {
+        $key = [Console]::ReadKey($true).Key
+        if ($key -eq 'Q') {
+            Write-Host "Stopping all processes..."
+            foreach ($p in $procs) {
+                if (!$p.HasExited) {
+                    $p.Kill()
+                }
+            }
+            break
+        }
+    }
+    Start-Sleep -Milliseconds 100
+}
